a69c73c47c72264df5345d6a6fbccfaf985abb6d
ChenJH <junhongchen@rastar.com>
2022-08-22 17:59:46 +0800

    cocoa pods

---
+++ JHHUD.podspec
@@ -0,0 +1,32 @@
+
+Pod::Spec.new do |spec|
+
+
+  spec.name         = "JHHUD"
+  
+  spec.version      = "1.0.0"
+  
+  spec.summary      = "A simple HUD with iOS."
+
+  spec.description  = "A simple HUD with iOS, you can use in your project."
+
+  spec.homepage     = "https://github.com/Zero-Cjh"
+  
+  spec.license      = { :type => "MIT", :file => "FILE_LICENSE" }
+
+  spec.author             = { "Zero-Cjh" => "876832130@qq.com" }
+
+  spec.platform     = :ios, "13.0"
+
+  spec.source       = { :git => "https://github.com/Zero-Cjh/JHHUD.git", :tag => "#{spec.version}" }
+
+  spec.source_files  = "Sources/JHHUD/*.swift"
+
+  spec.framework = 'UIKit', 'Foundation'
+  spec.dependency 'SnapKit'
+  
+  spec.requires_arc = true
+  
+  spec.swift_versions = ['5.0']
+
+end
---
+++ auto.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+script_dir="$( cd "$( dirname "$0"  )" && pwd  )"
+
+WORKSPACE_NAME="JHHUD"
+PROJECT_NAME=$WORKSPACE_NAME
+
+FRAMEWORK_NAME="JHHUD"
+FRAMEWORK_PATH=$script_dir/'Framework'
+FRAMEWORK_BUILD_OS_PATH=$FRAMEWORK_PATH/'Release-iphoneos'
+FRAMEWORK_BUILD_SIMULATOR_PATH=$FRAMEWORK_PATH/'Release-iphonesimulator'
+FRAMEWORK_OS_PATH=$FRAMEWORK_BUILD_OS_PATH/$FRAMEWORK_NAME.framework
+FRAMEWORK_SIMULATOR_PATH=$FRAMEWORK_BUILD_SIMULATOR_PATH/$FRAMEWORK_NAME.framework
+
+function buildClean() {
+    xcodebuild clean -workspace ${WORKSPACE_NAME}.xcworkspace \
+    -scheme ${PROJECT_NAME}
+}
+
+function buildOS() {
+    xcodebuild -workspace ${WORKSPACE_NAME}.xcworkspace \
+    -scheme ${PROJECT_NAME} \
+    ONLY_ACTIVE_ARCH=NO \
+    -configuration 'Release' \
+    -sdk iphoneos \
+    SYMROOT=${FRAMEWORK_PATH}
+    
+    buildClean
+}
+
+function buildSimulator() {
+    xcodebuild -workspace ${WORKSPACE_NAME}.xcworkspace \
+    -scheme ${PROJECT_NAME} \
+    ONLY_ACTIVE_ARCH=NO \
+    -configuration 'Release' \
+    -sdk iphonesimulator \
+    SYMROOT=${FRAMEWORK_PATH}
+    
+    buildClean
+}
+
+function lipoCreate() {
+    lipo -remove arm64 "$FRAMEWORK_SIMULATOR_PATH/$FRAMEWORK_NAME" -output "$FRAMEWORK_SIMULATOR_PATH/$FRAMEWORK_NAME"
+    lipo -create "$FRAMEWORK_OS_PATH/$FRAMEWORK_NAME" "$FRAMEWORK_SIMULATOR_PATH/$FRAMEWORK_NAME" -output "$FRAMEWORK_OS_PATH/$FRAMEWORK_NAME"
+    cp -rf "$FRAMEWORK_SIMULATOR_PATH/Modules" "$FRAMEWORK_OS_PATH"
+    cp -rf $FRAMEWORK_OS_PATH $FRAMEWORK_PATH
+    rm -rf $FRAMEWORK_BUILD_OS_PATH
+    rm -rf $FRAMEWORK_BUILD_SIMULATOR_PATH
+}
+buildOS
+buildSimulator
+lipoCreate
+
+open $FRAMEWORK_PATH
